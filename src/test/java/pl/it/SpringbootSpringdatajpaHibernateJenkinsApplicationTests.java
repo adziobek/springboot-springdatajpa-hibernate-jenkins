616592b349116f5d6fb5896260bbc838f77667ad
adziobek <adziobekit@gmail.com>
2022-03-03 14:25:29 +0100

    initial commit

---
+++ src/test/java/pl/it/SpringbootSpringdatajpaHibernateJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSpringdatajpaHibernateJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
